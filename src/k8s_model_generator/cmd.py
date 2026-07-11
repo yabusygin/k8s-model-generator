f641751d97cb7ee32ae4593d7b7b8bbece2d70f3
yabusygin <yaabusygin@gmail.com>
2026-07-12 02:59:17 +0300

    Output to Python package (#6)

--- src/k8s_model_generator/cmd.py
+++ src/k8s_model_generator/cmd.py
@@ -10,8 +10,9 @@ app = Typer()
 
 @app.command()
 def main(
-    output_dir: Annotated[Path, Argument(metavar="OUTPUT_DIR_PATH")],
+    output_dir: Annotated[Path, Argument(metavar="OUTPUT_DIRECTORY_PATH")],
     input_files: Annotated[list[Path], Argument(metavar="INPUT_FILE_PATH...")],
+    package_name: Annotated[str | None, Option(metavar="PACKAGE_NAME")] = None,
     no_snake_case_fields: Annotated[bool, Option("--no-snake-case-fields")] = False,
     python_version: Annotated[str, Option(metavar="VERSION")] = "minimal",
     pydantic_version: Annotated[str, Option(metavar="VERSION")] = "2.11",
@@ -19,6 +20,7 @@ def main(
     generate(
         input_files,
         output_dir,
+        package_name,
         not no_snake_case_fields,
         python_version,
         pydantic_version,
